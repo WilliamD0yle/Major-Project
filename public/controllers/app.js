e71561cde6d50b694fde4f85eb64545393a453c2
WilliamD0yle <william.doyle70@gmail.com>
2017-01-14 23:50:11 +0000

    fixing login and logout

--- public/controllers/app.js
+++ public/controllers/app.js
@@ -1,34 +1,80 @@
 'use strict';
-// Export the controller
+/********************************
+ Export the controller
+ ********************************/
 var app = angular.module('myApp', ['ngRoute']);
 
+//route configuration
 app.config(function ($routeProvider) {
-    $routeProvider
-        .when("/", {
+    
+    $routeProvider.
+        //root
+    when("/", {
+            templateUrl: "./views/home.html",
+            //controller: 'HomeController'
+        }).
+        //login page
+    when("/account/login", {
             templateUrl: "./views/login.html",
             controller: 'LoginController'
         }).
         //Create Account page
     when('/account/create', {
-            templateUrl: 'views/createAccount.html'
-                //controller: 'CreateAccountController'
+            templateUrl: 'views/createAccount.html',
+            controller: 'CreateAccountController'
+        }).
+    //Create Account page
+    when('/account/logout', {
+            templateUrl: 'views/logout.html',
+            controller: 'LogOutController'
         }).
         //Diary page
     when('/account/diary', {
-        templateUrl: 'views/diary.html'
-            //controller: 'CreateAccountController'
-    });
+        templateUrl: 'views/diary.html',
+        controller: 'DiaryController'
+        }).
+    otherwise({
+		redirectTo: '/account/login'
+      });
 });
 
-app.controller('LoginController', function ($scope, $location) {
+//login controller
+app.controller('LoginController', function ($scope, $location, $http) {
+    
+    // Login submission
+    $scope.submitLogin = function () {
+        // Login request
+        $http({
+                method: 'POST',
+                url: '/account/login',
+                data: {
+                    'username': $scope.loginForm.username,
+                    'password': $scope.loginForm.password
+                }
+            })
+            .success(function (response) {
+                $location.path('/account/diary');
+            })
+            .error(function () {
+                alert('Login failed. Check username/password and try again.');
+            });
+    };
     // Redirect to account creation page
     $scope.createAccount = function () {
         $location.path('/account/create');
     }
 });
 
-app.controller('CreateAccountController', function ($scope, $http) {
+//create account controller
+app.controller('CreateAccountController', function ($scope, $http, $location) {
 
+    $scope.gender = {};
+    var gender;
+    if ($scope.gender == "male") {
+        gender = "male";
+    } else {
+        gender = "female";
+    }
     // Create account
     $scope.submitForm = function () {
         $http({
@@ -40,7 +86,7 @@ app.controller('CreateAccountController', function ($scope, $http) {
                     'name': $scope.newUser.name,
                     'email': $scope.newUser.email,
                     'age': $scope.newUser.age,
-                    'gender': $scope.newUser.gender,
+                    'gender': gender,
                     'calories': $scope.newUser.calories
                 }
             })
@@ -82,3 +128,15 @@ app.controller('CreateAccountController', function ($scope, $http) {
             });
     };
 });
+
+//Diary
+app.controller('LogOutController', function ($scope, $location, $http) {
+ 
+});
+
+
+//Diary
+app.controller('DiaryController', function ($scope, $location, $http) {
+ 
+});
+
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 'use strict';
-// Dependencies
+/********************************
+ Dependencies 
+ ********************************/
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var morgan = require('morgan');
+var session = require('client-sessions');
+var User = require('./app/models/user.model');
 
 // Configs
 var db = require('./config/db');
@@ -17,8 +21,11 @@ mongoose.connect(db.url);
 // Initialize the Express App
 var app = express();
 
-// Configure 
-
+// Configure session
+app.use(session({
+  cookieName: 'session',
+  secret: 'random_string_goes_here'
+}));
 // To expose public assets to the world
 app.use(express.static(__dirname + '/public'));
 
@@ -31,9 +38,102 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-// Express Routes
-require('./app/routes/api')(app);
+//Express Routes
 require('./app/routes/routes')(app);
 
 // Start the app with listen and a port number
 app.listen(6001);
+
+/********************************
+Create Account
+********************************/
+app.post('/account/create', function (req, res) {
+
+    //Create new object that store's new user data
+    var newUser = new User({
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        gender: req.body.gender,
+        age: req.body.age,
+        calories: req.body.calories,
+        password: req.body.password
+    });
+
+    //Check if the username exists
+    User.findOne({username: req.body.username}, function (err, existingUser) {
+        //if the user exists
+        if (existingUser) {
+            return res.status(400).send('That username already exists. Please try a different username.');
+        }
+        //save the new user
+        newUser.save(function (err) {
+            //if there is an error send error message
+            if (err) {
+                console.log(err);
+                res.status(500).send('Error saving new account (database error). Please try again.');
+                return;
+            }
+            //send user the confirmation that the account has been created
+            res.status(200).send('Account created! Please login with your new account.');
+        });
+    });
+});
+
+/********************************
+Login to Account
+********************************/
+
+app.post('/account/login', function (req, res) {
+
+    var username = req.body.username;
+    var password = req.body.password;
+
+    //Check for the username in the mongo database
+    User.findOne({username: username, password: password}, function (err, user) {
+        //if there is an error
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error (database error). Please try again.');
+            return;
+        }
+        //if the user is not found
+        if(!user){
+            res.status(404).send('User not found. Please create an account to login.');
+            return;
+        }
+        //creating user session
+        req.session.user = username;
+        //user found send 200 success status 
+        res.status(200).send();
+        return; 
+    });
+});
+
+/********************************
+Log out
+********************************/
+
+app.get('/account/logout', function(req,res){
+    console.log(req.session.user);
+    // Destroys user's session
+    req.session.destroy();
+//    res.redirect('/account/login');
+});
+
+/********************************
+User Diary Page
+********************************/
+
+app.get('/account/diary', function (req, res) {
+    console.log(req.session.user);
+    //check if the user is logged in
+    if(!req.session){
+        res.status(401);
+        res.redirect('/account/login');
+    }
+    //user is logged in send 200 success status 
+    return res.status(200).send();  
+});
+
+
