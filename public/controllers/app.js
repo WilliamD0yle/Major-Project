dee4134acb1e1f441e579ee9a52294f70a7b2816
WilliamD0yle <william.doyle70@gmail.com>
2017-05-01 14:59:40 +0100

    added working barcode reader for smartphones

--- public/controllers/app.js
+++ public/controllers/app.js
@@ -58,6 +58,11 @@ app.config(function ($routeProvider) {
         templateUrl: 'views/progress.html',
         controller: 'ProgressController'
     }).
+    //Breakdown page
+    when('/test', {
+        templateUrl: 'views/cameraSearch.html',
+        controller: 'TestController'
+    }).
     otherwise({
         redirectTo: '/account/login'
     });
@@ -289,7 +294,6 @@ app.controller('ProgressController', function ($scope, $location, $http, $filter
         url: '/account/diary'
     }).
     success(function (response) {
-        console.log(response);
         //assign target the value of the users calories
         $scope.target = response.calories;
         $scope.progress();
@@ -866,165 +870,139 @@ app.controller('CustomMealController', function ($scope, $http, $route, $locatio
 
 //Search controller
 app.controller('SearchController', function ($scope, $location, $http, $route, Meal, ModalService) {
-    //start the camera
-    $scope.startQR = function () {
-        var resultCollector = Quagga.ResultCollector.create({
-            capture: true,
-            capacity: 20,
-            filter: function (codeResult) {
-                // only store results which match this constraint
-                // e.g.: codeResult
-                return true;
-            }
-        });
-        var App = {
-            init: function () {
-                var self = this;
+    var selectedMeal = {meal: Meal.Meal};
+    $scope.meal = selectedMeal.meal;
+    //redirect if the user isnt logged in
+    if(!$scope.meal){
+       $location.path('/');
+    }
+    //start the camera on desktop
+    var App = {
+        init: function () {
+            var self = this;
 
-                Quagga.init(this.state, function (err) {
-                    if (err) {
-                        return self.handleError(err);
-                    }
-                    Quagga.registerResultCollector(resultCollector);
-                    //Start the camera
-                    Quagga.start();
-                });
-            },
-            handleError: function (err) {
-                //log the error
-                console.log(err);
-            },
-            setState: function (path, value) {
-                var self = this;
-
-                if (typeof self._accessByPath(self.inputMapper, path) === "function") {
-                    value = self._accessByPath(self.inputMapper, path)(value);
+            Quagga.init(this.state, function (err) {
+                if (err) {
+                    return self.handleError(err);
                 }
-
-                self._accessByPath(self.state, path, value);
-                //console log the settigs
-                console.log(JSON.stringify(self.state));
-                App.detachListeners();
-                //Stop the camera
-                Quagga.stop();
                 //Start the camera
-                App.init();
+                Quagga.start();
+            });
+        },
+        handleError: function (err) {
+            //log the error
+            console.log(err);
+        },
+        initCameraSelection: function(){
+        var streamLabel = Quagga.CameraAccess.getActiveStreamLabel();
+
+        return Quagga.CameraAccess.enumerateVideoDevices().then(function(devices) {
+            function pruneText(text) {
+                return text.length > 30 ? text.substr(0, 30) : text;
+                }
+                var $deviceSelection = document.getElementById("deviceSelection");
+                while ($deviceSelection.firstChild) {
+                    $deviceSelection.removeChild($deviceSelection.firstChild);
+                }
+                devices.forEach(function(device) {
+                    var $option = document.createElement("option");
+                    $option.value = device.deviceId || device.id;
+                    $option.appendChild(document.createTextNode(pruneText(device.label || device.deviceId || device.id)));
+                    $option.selected = streamLabel === device.label;
+                    $deviceSelection.appendChild($option);
+                });
+            });
+        },
+        state: {
+            inputStream: {
+                name : "Live",
+                type : "LiveStream",
+                constraints: {
+                    //rare facing camera if availible 
+                    facingMode: "environment"
+                },
+                area: { // defines rectangle of the detection/localization area
+                    top: "0%", // top offset
+                    right: "0%", // right offset
+                    left: "0%", // left offset
+                    bottom: "0%" // bottom offset
+                },
+                singleChannel: false // true: only the red color-channel is read
             },
-            state: {
-                inputStream: {
-                    name : "Live",
-                    type : "LiveStream",
-                    constraints: {
-                        //rare facing camera if availible 
-                        facingMode: "environment"
-                    },
-                    area: { // defines rectangle of the detection/localization area
-                        top: "0%", // top offset
-                        right: "0%", // right offset
-                        left: "0%", // left offset
-                        bottom: "0%" // bottom offset
-                    },
-                    singleChannel: false // true: only the red color-channel is read
-                },
-                locator: {
-                    patchSize: "medium",
-                    halfSample: true
-                },
-                numOfWorkers: 4,
-                decoder: {
-                    readers: [{ //type of barcode then will be read - ean is used for food and beverage products
-                        format: "ean_reader",
-                        config: {
-                            //supplements: ['ean_8_reader']
-                        }
-                }]
-                },
-                locate: true
+            numOfWorkers: 4,
+            decoder: {
+                readers: [{ //type of barcode then will be read - ean is used for food and beverage products
+                    format: "ean_reader",
+                    config: {
+                        //supplements: ['ean_8_reader']
+                    }
+            }]
             },
-            lastResult: null
-        };
-        App.init();
-        //adds helper details to video on canvas to where its looking for the barcode
-        Quagga.onProcessed(function (result) {
-            var drawingCtx = Quagga.canvas.ctx.overlay,
-                drawingCanvas = Quagga.canvas.dom.overlay;
-
-            if (result) {
-                if (result.boxes) {
-                    drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-                    result.boxes.filter(function (box) {
-                        return box !== result.box;
-                    }).forEach(function (box) {
-                        Quagga.ImageDebug.drawPath(box, {
-                            x: 0,
-                            y: 1
-                        }, drawingCtx, {
-                            color: "green",
-                            lineWidth: 2
-                        });
-                    });
-                }
-
-                if (result.box) {
-                    Quagga.ImageDebug.drawPath(result.box, {
-                        x: 0,
-                        y: 1
-                    }, drawingCtx, {
-                        color: "#00F",
-                        lineWidth: 2
-                    });
-                }
-
-                if (result.codeResult && result.codeResult.code) {
-                    Quagga.ImageDebug.drawPath(result.line, {
-                        x: 'x',
-                        y: 'y'
-                    }, drawingCtx, {
-                        color: 'red',
-                        lineWidth: 3
-                    });
-                }
-            }
-        });
-
-        Quagga.onDetected(function (result) {
-            var code = result.codeResult.code;
-            jQuery(".video").hide();
-            jQuery(".enteredBarcode").html(code);
-            $scope.barcodeSearch(code);
-            Quagga.stop();
-        });
+            locate: true
+        }
     };
 
-    //load the load function
-    $scope.startQR();
+    App.init();
+	// Create the QuaggaJS config object for the live stream
+    var liveStreamConfig = {
+			inputStream: {
+				type : "LiveStream",
+				constraints: {
+					facingMode: "environment" // or "user" for the front camera
+				}
+			},
+			locator: {
+				patchSize: "medium",
+				halfSample: true
+			},
+			numOfWorkers: (navigator.hardwareConcurrency ? navigator.hardwareConcurrency : 4),
+			decoder: {
+				"readers":[
+					{"format":"ean_reader","config":{}}
+				]
+			},
+			locate: true,
+		};
     
-    $scope.$on("$locationChangeStart", function(event) {
-        Quagga.stop();
-    });
-
+	var fileConfig = $.extend({}, liveStreamConfig,{inputStream: {size: 800}});
+	
+	// Once a barcode had been read successfully, stop quagga
+	Quagga.onDetected(function(result) {  
+		if (result.codeResult.code){
+            Quagga.stop();	
+            $scope.barcodeSearch(result.codeResult.code);		
+		}
+	});
+	// file input
+	$(".container input:file").on("change", function(e) {
+        Quagga.stop();	
+		if (e.target.files && e.target.files.length) {
+			Quagga.decodeSingle($.extend({}, fileConfig, {src: URL.createObjectURL(e.target.files[0])}), function(result) {});
+		}
+	});
+    
     $scope.barcodeSearch = function (barcode) {
-
+        var search = true;
         var searchURL = "https://world.openfoodfacts.org/api/v0/product/" + barcode + ".json";
-        $http({
+        if(search){
+            $http({
             method: 'GET',
             url: searchURL,
-        }).
-        success(function (response) {
-            if(response.product.complete == 0){
-                alert("Product not found!");  
-                $location.path('/account/diary');
-               }else{
-                $scope.foodInformation(response.product);  
-               }
-        }).
-        error(function (response) {
-            console.log(response);
-        });
+            }).
+            success(function (response) {
+                if(response.product.complete == 0){
+                    alert("Product not found!");  
+                    $location.path('/account/diary');
+                   }else{
+                    $scope.foodInformation(response.product);  
+                   }
+            }).
+            error(function (response) {
+                console.log(response);
+            });
+            search = false;
+        } 
     };
-    
-    var selectedMeal = {meal: Meal.Meal};
-    $scope.meal = selectedMeal.meal; 
     
     $scope.foodInformation = function (food) {
         ModalService.showModal({
